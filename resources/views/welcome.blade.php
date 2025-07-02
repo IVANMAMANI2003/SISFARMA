8624a4fbcbe8fb97c977395b209951265dfa6733
IVANMAMANI2003 <82121615+IVANMAMANI2003@users.noreply.github.com>
2025-07-02 15:49:21 -0500

    subiendo prueba 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,6 +66,7 @@
             <li><a href="#hero" class="active">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
+            <li><a href="#services">Services</a></li>
             <li><a href="#departments">Departments</a></li>
             <li><a href="#departments">Departments</a></li>
             <li><a href="#doctors">Doctors</a></li>
